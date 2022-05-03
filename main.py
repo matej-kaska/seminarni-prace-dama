0d15c5fb6ce2b7d95ecfa537bde4aef2508014f9
matej.kaska <nkaskaj@gmail.com>
2022-05-03 14:22:58 +0200

    merge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import math
-from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT
+from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT, YELLOW
 from checkers.board import Board
 
 FPS = 60
@@ -10,21 +10,39 @@ pygame.display.set_caption("Checkers")
 board = Board()
 
 def main():
+    selected = False
     run = True
     clock = pygame.time.Clock()
     board.create_board()
 
     while run:
         clock.tick(FPS)
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                
+                if selected == True:
+                    if board.squares[y][x].piece is not None:
+                        board.squares[y][x].piece._color = board.squares[y][x].piece._defaultColor
+                        BeforeY = y
+                        BeforeX = x
+                        x, y = pygame.mouse.get_pos()
+                        x = math.floor(x / SQUARE_SIZE)
+                        y = math.floor(y / SQUARE_SIZE)
+                        if board.squares[y][x].piece is None:
+                            board.squares[y][x].piece = board.squares[BeforeY][BeforeX].piece
+                            board.squares[BeforeY][BeforeX].piece = None
+                            board.squares[y][x].piece._color = board.squares[y][x].piece._defaultColor
+                            selected = False
+                            break
+
                 x, y = pygame.mouse.get_pos()
                 x = math.floor(x / SQUARE_SIZE)
                 y = math.floor(y / SQUARE_SIZE)
-                board_click(x, y)
+                if board.squares[y][x].piece is not None:
+                    board.squares[y][x].piece._color = YELLOW
+                    selected = True
 
         board.draw_board(WIN)
         pygame.display.update()
@@ -33,7 +51,8 @@ def main():
 
 def board_click(x, y):
     print(y, x)
-    print(board.squares[y][x].piece)
+    print(board.squares[y][x].piece._x)
+    print(board.squares[y][x].piece._y)
 
 if __name__ == "__main__":
     main()
