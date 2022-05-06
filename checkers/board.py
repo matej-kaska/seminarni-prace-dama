2ec855bb4cf46c7c85a414d7a286390aa382cf74
M3chro <jakub.havel7@gmail.com>
2022-05-06 22:19:51 +0200

    doladeny csv vyjimky

--- checkers/board.py
+++ checkers/board.py
@@ -40,7 +40,21 @@ class Board:
         try:
             file = open('data.csv')
             csvreader = csv.reader(file)
-            dict = { rows[0].strip():rows[1].strip() for rows in csvreader if rows }        
+            dict = { rows[0].strip().upper():rows[1].strip().lower() for rows in csvreader if "".join(rows).strip() } 
+            labels = (
+                "A1", "A3", "A5", "A7",
+                "B2", "B4", "B6", "B8",
+                "C1", "C3", "C5", "C7",
+                "D2", "D4", "D6", "D8",
+                "E1", "E3", "E5", "E7",
+                "F2", "F4", "F6", "F8",
+                "G1", "G3", "G5", "G7",
+                "H2", "H4", "H6", "H8"
+            )
+            pieces = ("b", "bb", "w", "ww")
+            for label, piece in dict.items():
+                if label not in labels or piece not in pieces:
+                    raise Exception("Wrong format")  
             if len(dict) > 24: raise Exception("Too many pieces")                 
             label = ""
             for i in range(COLS):
